3caf4d4cd23a0bf9d4caaffa33c2703368e77728
Youngestage <youngleslie.ik@gmail.com>
2026-03-24 17:53:24 +0100

    pixel perfect changes to dashboard

--- src/app/dashboard/user-mgt/customers/page.tsx
+++ src/app/dashboard/user-mgt/customers/page.tsx
@@ -1,7 +1,7 @@
 export default function CustomersPage() {
   return (
     <>
-      <h1 className="text-primary-text text-[34px] font-semibold">
+      <h1 className="text-primary-text text-[20px] font-semibold">
         Customers
       </h1>
       <p className="mt-2 text-zinc-600">Customers (mock).</p>
